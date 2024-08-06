147f3b2bfa5bd8a1a197a49667c0bcf4573d5104
johnr14 <5272079+johnr14@users.noreply.github.com>
2024-08-06 11:43:30 -0400

    Fix(firefox): fixed scripts to generate profiles.ini and .desktop icons; TODO: change icons

--- dot_mozilla/private_firefox/executable_generate_desktop_icons.sh
+++ dot_mozilla/private_firefox/executable_generate_desktop_icons.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
 # Set the path to the Firefox profiles directory
-PROFILE_DIR=~/.mozilla/firefox
+PROFILE_DIR="$HOME/.mozilla/firefox"
 
 # Set the path to the Desktop directory
-DESKTOP_DIR=~/Desktop
+DESKTOP_DIR="$HOME/Desktop"
 
 # Loop through each profile entry in profiles.ini
 while IFS='=' read -r key value; do
   # Check if the key is "Path" and the value contains a dot (.)
   if [ "$key" == "Path" ] && [[ "$value" == *.* ]]; then
     # Extract the profile name from the path
-    profile_name="${value##*.}"
+    profile_name="$(basename ${value##*.})"
+    echo $profile_name
     # Create the .desktop file
     echo "[Desktop Entry]" > "$DESKTOP_DIR/FF-$profile_name.desktop"
     echo "Type=Application" >> "$DESKTOP_DIR/FF-$profile_name.desktop"
-    echo "Name=$profile_name" >> "$DESKTOP_DIR/FF-$profile_name.desktop"
+    echo "Name=FF-${profile_name}" >> "$DESKTOP_DIR/FF-$profile_name.desktop"
     echo "Comment=my $profile_name profile" >> "$DESKTOP_DIR/FF-$profile_name.desktop"
-    echo "Exec=firefox -Profile $value -no-remote" >> "$DESKTOP_DIR/FF-$profile_name.desktop"
+    echo "Exec=firefox -Profile ${PROFILE_DIR}/$value -no-remote" >> "$DESKTOP_DIR/FF-$profile_name.desktop"
     echo "Path=~/." >> "$DESKTOP_DIR/FF-$profile_name.desktop"
     echo "Icon=firefox" >> "$DESKTOP_DIR/FF-$profile_name.desktop"
     echo "Terminal=false" >> "$DESKTOP_DIR/FF-$profile_name.desktop"
     echo "StartupNotify=false" >> "$DESKTOP_DIR/FF-$profile_name.desktop"
   fi
 done < "$PROFILE_DIR/profiles.ini"
+chmod u+x ~/Desktop/FF*.desktop
--- dot_mozilla/private_firefox/executable_generate_profile_ini.sh
+++ dot_mozilla/private_firefox/executable_generate_profile_ini.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the path to the Firefox profiles directory
-PROFILE_DIR=~/.mozilla/firefox
+PROFILE_DIR="$HOME/.mozilla/firefox"
 
 # Delete old profiles.ini if it exists
 [ -f "$PROFILE_DIR/profiles.ini" ] && rm "$PROFILE_DIR/profiles.ini"
@@ -15,15 +15,16 @@ PROFILE_COUNT=0
 # Loop through each profile directory
 for profile in "$PROFILE_DIR"/*; do
   # Skip non-directory entries
-  [ -d "$profile" ] && [[ "$profile" == *.* ]] || continue
-
-  # Write the profile entry to profiles.ini
-  echo "[Profile$PROFILE_COUNT]" >> "$PROFILE_DIR/profiles.ini"
-  echo "Name=${profile##*.}" >> "$PROFILE_DIR/profiles.ini"
-  echo "IsRelative=1" >> "$PROFILE_DIR/profiles.ini"
-  echo "Path=$profile" >> "$PROFILE_DIR/profiles.ini"
-  echo "" >> "$PROFILE_DIR/profiles.ini" # Add a blank line between profiles
-
+  #PROFILE_NAME=$(basename "$profile") 
+  if [ -d "$profile" ] && [[ "$profile" =~ $(basename [0-9a-z]{8}\.[a-zA-Z0-9\-]+$) ]]; then
+   echo $profile
+   # Write the profile entry to profiles.ini
+   echo "[Profile$PROFILE_COUNT]" >> "$PROFILE_DIR/profiles.ini"
+   echo "Name=${profile##*.}" >> "$PROFILE_DIR/profiles.ini"
+   echo "IsRelative=0" >> "$PROFILE_DIR/profiles.ini"
+   echo "Path=$PROFILE_DIR/$(basename ${profile})" >> "$PROFILE_DIR/profiles.ini"
+   echo "" >> "$PROFILE_DIR/profiles.ini" # Add a blank line between profiles
   # Increment profile counter
   PROFILE_COUNT=$((PROFILE_COUNT + 1))
+  fi
 done
